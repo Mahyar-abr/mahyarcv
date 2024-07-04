4f590c668f02c55968d26c99914945b5bba968f5
mahyar <mahyarabr96@gmail.com>
2024-07-04 13:28:09 +0330

    ssg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 
 export default {
-  plugins: [react(), vike()],
+  plugins: [react(), vike({ prerender: true })],
   base: "/mahyarcv/",
 }
